40c0e33442a7f9fb75495391505d89e305627137
Laszlo Tamas <tamas.lacko@gmail.com>
2016-10-21 08:09:52 +0200

    Fix wrongly calculated size for thumbnail if width/height values are percentage based

--- dist/ng-file-upload.js
+++ dist/ng-file-upload.js
@@ -1095,13 +1095,13 @@ ngFileUpload.directive('ngfSelect', ['$parse', '$timeout', '$compile', 'Upload',
             size = {width: elem[0].naturalWidth || elem[0].clientWidth,
               height: elem[0].naturalHeight || elem[0].clientHeight};
           }
-          if (size.width === 0 && window.getComputedStyle) {
-            var style = getComputedStyle(elem[0]);
-            size = {
-              width: parseInt(style.width.slice(0, -2)),
-              height: parseInt(style.height.slice(0, -2))
-            };
-          }
+          // if (size.width === 0 && window.getComputedStyle) {
+          //   var style = getComputedStyle(elem[0]);
+          //   size = {
+          //     width: parseInt(style.width.slice(0, -2)),
+          //     height: parseInt(style.height.slice(0, -2))
+          //   };
+          // }
         }
 
         if (angular.isString(file)) {
--- src/data-url.js
+++ src/data-url.js
@@ -141,13 +141,13 @@
             size = {width: elem[0].naturalWidth || elem[0].clientWidth,
               height: elem[0].naturalHeight || elem[0].clientHeight};
           }
-          if (size.width === 0 && window.getComputedStyle) {
-            var style = getComputedStyle(elem[0]);
-            size = {
-              width: parseInt(style.width.slice(0, -2)),
-              height: parseInt(style.height.slice(0, -2))
-            };
-          }
+          // if (size.width === 0 && window.getComputedStyle) {
+          //   var style = getComputedStyle(elem[0]);
+          //   size = {
+          //     width: parseInt(style.width.slice(0, -2)),
+          //     height: parseInt(style.height.slice(0, -2))
+          //   };
+          // }
         }
 
         if (angular.isString(file)) {
